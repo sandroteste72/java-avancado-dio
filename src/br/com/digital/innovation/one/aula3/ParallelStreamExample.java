c6fb4ff0ba600ec1dc183f56ce68e0489247a6c3
sandroteste72 <sandro.teste72@gmail.com>
2021-08-18 03:48:01 -0300

    Arrumando o codigo 3

--- src/br/com/digital/innovation/one/aula3/ParallelStreamExample.java
+++ src/br/com/digital/innovation/one/aula3/ParallelStreamExample.java
@@ -7,29 +7,26 @@ import java.util.stream.IntStream;
 
 public class ParallelStreamExample {
     public static void main(String[] args) {
-//        long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 //        IntStream.range(1,10000).forEach(num -> System.out.println(factorial(num)));
-//        IntStream.range(1,100000).forEach(num -> factorial(num)); // Serial stream
-//        long fim = System.currentTimeMillis();
-//        System.out.println("Runtime Serial :: "+(fim-start));
-//
-//        start = System.currentTimeMillis();
-//        IntStream.range(1,10000).forEach(num -> System.out.println(factorial(num)));
-//        IntStream.range(1,100000).forEach(num -> factorial(num)); // Parallel stream
-//        end = System.currentTimeMillis();
-//        System.out.println("Runtime Parallel :: "+(end-start));
+        IntStream.range(1,100000).forEach(num -> factorial(num)); // Serial stream
+        long end = System.currentTimeMillis();
+        System.out.println("Runtime Serial :: "+(end - start));
+
+        start = System.currentTimeMillis();
+        IntStream.range(1,100000).parallel().forEach(num -> factorial(num)); // Parallel stream
+        end = System.currentTimeMillis();
+        System.out.println("Runtime Parallel :: " + (end - start));
 
 //        List<String> names = Arrays.asList("Joao","Paulo","Oliveira","Santos");
-        List<String> names = Arrays.asList("Joao","Oliveira","Paulo","Santos");
-        names.parallelStream().forEach(System.out::println);
-
-
+//        List<String> names2 = Arrays.asList("Joao","Oliveira","Paulo","Santos");
+//        names.parallelStream().forEach(System.out::println);
+//        names2.parallelStream().forEach(System.out::println);
     }
 
     public static long factorial(long number){
         long fat = 1;
-
-        for (long i = 2; i <- number ; i++) {
+        for (long i = 2; i <= number ; i++) {
             fat *= i;
         }
         return fat;
