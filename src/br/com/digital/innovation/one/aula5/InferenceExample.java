514d4dd3edef472828068f1c9fd4f2de953d3185
sandroteste72 <sandro.teste72@gmail.com>
2021-08-20 02:05:18 -0300

    Correcting the code

--- src/br/com/digital/innovation/one/aula5/InferenceExample.java
+++ src/br/com/digital/innovation/one/aula5/InferenceExample.java
@@ -9,129 +9,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-//import java.net.URLConnection;
+import java.net.URLConnection;
 import java.util.stream.Collectors;
-import java.util.Formatter;
 
 public class InferenceExample {
-
-//    private var joao = new String("joao"); // var não consegue utilizar a nível de classe, só variáveis locais, de preferencia dentro de um for ou métodos.
-
 //    public static void main(String[] args) throws IOException {
-////        URL url = new URL("Https://docs.oracle.com/javase/10/language/");
-////        URLConnection urlConnection = url.openConnection();
-////        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((urlConnection.getInputStream())));
+        //It can be done that way
+//        URL url = new URL("Https://docs.oracle.com/javase/10/language/");
+//        URLConnection urlConnection = url.openConnection();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((urlConnection.getInputStream())));
 //
+        //Or can be done that way
 //        var url = new URL("Https://docs.oracle.com/javase/10/language/");
 //        var urlConnection = url.openConnection();
 //        var bufferedReader = new BufferedReader(new InputStreamReader((urlConnection.getInputStream())));
 //
 //        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">",">\n"));
-
-
-//    public static void main(String[] args) throws IOException {
-//        testar("Joao","Santos");
-//    }
-
-//    public static void main(String[] args) throws IOException {
-//        printarSoma(5, 5, 5);
 //    }
 
     public static void main(String[] args) {
         connectAndPrintURLJavaOracle();
+        printFullName("Sandro", "Arpi");
+        printSum(15, 18);
     }
 
+        private static void connectAndPrintURLJavaOracle () {
+            try {
+                var url = new URL("https://docs.oracle.com/javase/10/language/");
+                var urlConnection = url.openConnection();
 
-//    private static void connectAndPrintURLJavaOracle() throws IOException {
-//        var url = new URL("Https://docs.oracle.com/javase/10/language/");
-//        var urlConnection = url.openConnection();
-//        var bufferedReader = new BufferedReader(new InputStreamReader((urlConnection.getInputStream())));
-//        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
-//    }
-
-    private static void connectAndPrintURLJavaOracle() {
-
-        try {
-             var url = new URL("Https://docs.oracle.com/javase/10/language/");
-             var urlConnection = url.openConnection();
-
-             try (var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
-                 System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">",">\n"));
-             }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-//    public void testar() {
-
-//    public static void testar(String nome) {
-////    public static void testar(var nome) { // var não pode ser utilizado em nível de class ou como parametro
-//
-//        System.out.println(nome);
-//    }
-
-//    public static void testar(String nome, String sobrenome) {
-//        var nomeCompleto = String.format("%s %s",nome,sobrenome);
-//        System.out.println(nomeCompleto);
-//
-//    }
-
-    public static void printarNomeCompleto(String nome, String sobrenome) {
-        var nomeCompleto = String.format("%s %s", nome, sobrenome);
-        System.out.println(nomeCompleto);
-    }
-
-//    public static void printarSoma(int a, int b) {
-//        var soma = a+b;
-//
-//        System.out.println(soma);
-//    }
-
-//    public static void printarSoma(int... numeros) {
-//        int soma;
-//        if (numeros.length > 0) {
-//            soma = 0;
-//            for (int numero : numeros) {
-//                soma += numero;
-//            }
-//            System.out.println("A soma é ::: "+soma);
-//        }
-//    }
-
-    public static void printarSoma(int... numeros) {
-        int soma;
-//        var soma;
-        if (numeros.length > 0) {
-            soma = 0;
-//            for (var numero : numeros) {
-//                soma += numero;
-//            }
-
-//            for (int numero = 0; numero < numeros.length; numero++) {
-            for (var numero = 0; numero < numeros.length; numero++) {
-                soma+=numeros[numero];
+                try (var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                    System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println("A soma é ::: "+soma);
+        }
+
+    public static void printFullName(String name, String lastName) {
+        var fullName = String.format("%s %s", name, lastName);
+        System.out.println(fullName);
+    }
+
+    public static void printSum(int... numbers) {
+        int sum;
+        if (numbers.length > 0) {
+            sum = 0;
+            for (var number = 0; number < numbers.length; number++) {
+                sum += numbers[number];
+            }
+            System.out.println("A soma e ::: " + sum);
         }
     }
 
-//     Consegue
-//
-//        variáveis locais inicializadas
-//        variável suporte do enhaced for
-//        variável suporte do for interativo
-//        variável try with resouce
-//
-//
-//     Não consegue
-//
-//         var não pode ser utilizado em nível de class
-//         var não pode ser utilizado como parametro
-//         var não pode ser utilizado em variáveis locais não inicializadas
-//
-//     Link para estudos: Https://docs.oracle.com/javase/10/language/
+        //Consegue
 
+        // variaveis local inicializadas
+        // variavel suporte do enhaced for
+        // variavel suporte do for iterativo
+        // variavel try with resource
+
+        //Nao consegue
+
+        // var nao pode ser utilizado em nivel de classe
+        // var nao pode ser utilizado como parametro
+        // var nao pode ser utilizada em variaveis locais nao inicializadas
+
+
+        // https://docs.oracle.com/javase/10/language/
 }
 
 //AULA DOCKER
@@ -150,4 +95,3 @@ public class InferenceExample {
 //        $ /exit
 //        $ sudo docker run -it --cpus 1 openjdk:10-jdk
 //        $ Runtime.getRuntime().availableProcessors()
-
